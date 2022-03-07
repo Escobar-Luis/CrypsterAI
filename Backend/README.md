8f9e5131980d57da98293ec74b17de225c15d3e3
Luis Escobar <lae311@stern.nyu.edu>
2022-03-07 11:58:11 -0500

    s

---
+++ Backend/README.md
@@ -0,0 +1,1 @@
+# CrypsterAI_API
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { InMemoryCache, ApolloClient, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://127.0.0.1:8000/graphql",
+  uri: "https://crypsterai.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
